37db0ca84e49c128d03e81d61a6b11cb85b104fd
Andrew Peterson <andy@ndpsoftware.com>
2015-04-25 14:21:55 -0700

    ?

--- gesture_spy.js
+++ gesture_spy.js
@@ -1,74 +1,18 @@
-var _gaq = _gaq || [];
-
 var GestureSpy = (function () {
 
   var report;
 
-  function getDistance(pos1, pos2) {
-    var x = pos2.x - pos1.x, y = pos2.y - pos1.y;
-    return Math.sqrt((x * x) + (y * y));
-  }
+//  function getDistance(pos1, pos2) {
+//    var x = pos2.x - pos1.x, y = pos2.y - pos1.y;
+//    return Math.sqrt((x * x) + (y * y));
+//  }
 
-  var eventTarget = function (event) {
-    function nodeName(node) {
-      s = '';
-      if (node) {
-        if (node.id) {
-          s = '#'+ node.id;
-        } else if (node.nodeName == 'BODY' || node.nodeName == 'HTML') {
-          return 'body';
-        } else {
-          s = nodeName(node.parentNode);
-          if (s) s += '>';
-          s += node.nodeName.toLowerCase();
-          if (node.className) s += '.' + node.className;
-        }
-      }
-      return s;
-    }
+  var _lastRotation = null; // just a value we use to filter out duplicates
 
-    return  nodeName(event.target);
-  }
-
-  var eventValue = function (event) {
-    var name = eventName(event);
-    if (name == 'rotate') return event.rotation;
-    if (name == 'pinch') return event.scale;
-    if (name == 'zoom') return event.scale;
-    return name;
-  }
-
-  var eventName = function (event) {
-    var rot = Math.abs(event.rotation);
-    var scale = event.scale;
-    if (rot < 20) {
-      if (scale > 1.1) return 'zoom';
-      if (scale < 0.9) return 'pinch';
-      return 'swipe';
-    } else {
-      return 'rotate';
-    }
-  }
-
-  var lastRotation = null; // just a value we use to filter out duplicates
-  var gestureEnd = function (event) {
-    if (lastRotation !== event.rotation) {
-      report(eventName(event), eventTarget(event), eventValue(event));
-    }
-    lastRotation = event.rotation;
-  }
-
-  var orientationChange = function (event) {
-    var orientation = window.orientation % 180 ? 'landscape' : 'portrait';
-    report('orientation change', orientation);
-  }
-
-  return {
+  var gestureSpy = {
     spy: function (options) {
-      report = (options && options.report) || function (evt, label, value) {
-        _gaq.push(['_trackEvent', 'GestureSpy', evt, label, value]);
-      };
-      setTimeout(function() {
+      report = (options && options.report) || gestureSpy.reportGA;
+      setTimeout(function () {
         var bodies = document.getElementsByTagName('BODY');
         for (var i = 0; i < bodies.length; i++) {
           var body = bodies[i];
@@ -76,16 +20,67 @@ var GestureSpy = (function () {
           body.addEventListener('click', function () {
             void(0)
           });
-          body.addEventListener('gestureend', gestureEnd, false);
-          body.addEventListener('orientationchange', orientationChange, false);
+          body.addEventListener('gestureend', gestureSpy.gestureEnd, false);
+          body.addEventListener('orientationchange', gestureSpy.orientationChange, false);
         }
       }, 1000);
     },
-    eventName: eventName,
-    eventTarget: eventTarget,
-    eventValue: eventValue
+    eventName: function (event) {
+        var rot = Math.abs(event.rotation);
+        var scale = event.scale;
+        if (rot < 20) {
+          if (scale > 1.1) return 'zoom';
+          if (scale < 0.9) return 'pinch';
+          return 'swipe';
+        } else {
+          return 'rotate';
+        }
+      },
+    eventTarget: function (event) {
+        function nodeName(node) {
+          var s = '';
+          if (node) {
+            if (node.id) {
+              s = '#' + node.id;
+            } else if (node.nodeName == 'BODY' || node.nodeName == 'HTML') {
+              return 'body';
+            } else {
+              s = nodeName(node.parentNode);
+              if (s) s += '>';
+              s += node.nodeName.toLowerCase();
+              if (node.className) s += '.' + node.className;
+            }
+          }
+          return s;
+        }
+
+        return nodeName(event.target);
+      },
+    eventValue: function (event) {
+        var name = gestureSpy.eventName(event);
+        if (name == 'rotate') return event.rotation;
+        if (name == 'pinch') return event.scale;
+        if (name == 'zoom') return event.scale;
+        return name;
+      },
+    gestureEnd: function (event) {
+        if (_lastRotation !== event.rotation) {
+          report(gestureSpy.eventName(event),
+              gestureSpy.eventTarget(event),
+              gestureSpy.eventValue(event));
+        }
+        _lastRotation = event.rotation;
+      },
+    orientationChange: function () {
+        var orientation = window.orientation % 180 ? 'landscape' : 'portrait';
+        report('orientation change', orientation);
+      },
+    reportGA: function (evt, label, value) {
+        _gaq.push(['_trackEvent', 'GestureSpy', evt, label, value]);
+      }
   };
 
+  return gestureSpy;
 })();
 
 
--- spec/gesture_spy_spec.js
+++ spec/gesture_spy_spec.js
@@ -2,7 +2,7 @@ describe("gestureSpy", function () {
   it("should run without errors", function () {
     GestureSpy.spy();
   });
-  describe('#eventName', function() {
+  describe('#eventName', function () {
     it("should return rotate if rotation is < -20", function () {
       expect(GestureSpy.eventName({rotation: -21, scale: 1})).toEqual('rotate');
     });
@@ -15,7 +15,7 @@ describe("gestureSpy", function () {
     it("should return zoom if rotation is > -20 and scale > 1.1", function () {
       expect(GestureSpy.eventName({rotation: -19, scale: 1.1001 })).toEqual('zoom');
     });
-    it("should report drag if -20 < rotation < 20 and .9 < scale < 1.1", function() {
+    it("should report drag if -20 < rotation < 20 and .9 < scale < 1.1", function () {
       expect(GestureSpy.eventName({rotation: -19, scale: 0.91 })).toEqual('swipe');
       expect(GestureSpy.eventName({rotation: 19, scale: 0.91 })).toEqual('swipe');
       expect(GestureSpy.eventName({rotation: -19, scale: 1.09 })).toEqual('swipe');
@@ -35,6 +35,43 @@ describe("gestureSpy", function () {
     });
     xit("should return distance from swipe", function () {
       expect(GestureSpy.eventValue({rotation: 19, scale: 0.91 })).toEqual(0);
+    });
+  });
+
+  describe("#eventTarget", function () {
+    var dom, divWithId, divWithClass, span;
+    beforeEach(function() {
+      divWithId = document.createElement('DIV');
+      divWithId.setAttribute('id','hah');
+
+      divWithClass = document.createElement('DIV');
+      divWithClass.setAttribute('class','heh');
+      divWithId.appendChild(divWithClass);
+
+      span = document.createElement('SPAN');
+      divWithClass.appendChild(span);
+    });
+    it("should return ID", function () {
+      var event = {};
+      event.target = divWithId;
+      expect(GestureSpy.eventTarget(event)).toEqual('#hah')
+    });
+    it("should return parent + .class", function () {
+      var event = {};
+      event.target = divWithClass;
+      expect(GestureSpy.eventTarget(event)).toEqual('#hah>div.heh')
+    });
+    it("should return parent + .class ...", function () {
+      var event = {};
+      event.target = span;
+      expect(GestureSpy.eventTarget(event)).toEqual('#hah>div.heh>span')
+    });
+  });
+  describe("reportGA", function () {
+    it("should push onto _gaq", function () {
+      _gaq = [];
+      GestureSpy.reportGA('a', 'b', 'c');
+      expect(_gaq[0]).toEqual(['_trackEvent', 'GestureSpy', 'a', 'b', 'c']);
     });
   });
 });
